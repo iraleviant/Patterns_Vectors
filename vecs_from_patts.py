1e698ff4e39160a88f4f15badc7cad35a7d01411
iraleviant <ira.krayze@gmail.com>
2019-01-23 12:15:03 -0800

    Update vecs_from_patts.py

--- vecs_from_patts.py
+++ vecs_from_patts.py
@@ -6,12 +6,13 @@ import numpy as np
 from scipy.sparse import csr_matrix, lil_matrix
 #from scipy import sparse
 import scipy.sparse as ss
+import time
 
 PATT_STR="PATT"
 CW_SYMBOL="CW"
-PATT_ELEMENTS_SEPERATOR="-"
-HIGH_FREQUENCY_THR = 0.2#0.002#use constant HIGH_FREQUENCY_THR => 0.002; orig, mine_test=0.8
-MIN_FREQ =3#200 #orig=100, mine_test=3
+PATT_ELEMENTS_SEPERATOR=" "
+HIGH_FREQUENCY_THR =0.002  #0.002 #0.2#0.002#use constant HIGH_FREQUENCY_THR => 0.002; orig, mine_test=0.8
+MIN_FREQ =100 #100 #3#200 #orig=100, mine_test=3
 
 ### code assumes lower case corpus 
 
@@ -136,7 +137,7 @@ def add_patt_instance(words, start, patt_index, patterns_trie, cws_clean_order, 
     # Return if word is empty or punctuation, same as in function get_cws, check it
     #if len(word)==0 or not re.match(r'^[a-zA-Z]+$', word):  #r'\b[a-zA-Z]+\b', \b-# Assert position at a word boundary
     #match=re.search(r'^[^a-z]+$', words[start])
-    elif  not bool(re.match(r'^[a-z]+$', words[start])) or not(len(words[start])) :
+    elif  not bool(re.match(r'^[a-z_]+$', words[start])) or not(len(words[start])) :
         return
     
     
@@ -144,7 +145,7 @@ def add_patt_instance(words, start, patt_index, patterns_trie, cws_clean_order, 
     if words[start] in patterns_trie.node :
         substr=patterns_trie.sub_trie(words[start])
         add_patt_instance(words, start+1,patt_index+1, substr, cws_clean_order, co_mat, word_vocab, context_vocab, ofh)
-    elif (bool(re.search(r'^[a-z]+$', words[start]))) and (words[start] in cws_clean_order ) and (CW_SYMBOL in patterns_trie.node ) :
+    elif (bool(re.search(r'^[a-z_]+$', words[start]))) and (words[start] in cws_clean_order ) and (CW_SYMBOL in patterns_trie.node ) :
         substr=patterns_trie.sub_trie(CW_SYMBOL)
         add_patt_instance(words, start+1, patt_index+1, substr, cws_clean_order, co_mat, word_vocab, context_vocab, ofh)
         
@@ -174,9 +175,9 @@ def get_cws(files):
         with open(corpus_file, 'r') as ifh:
             for line in ifh:
                 n_sent+=1
-                if n_sent % 10000 == 0: #n_sent divides in 10000 without remainder
-                    print  (str(round(float(n_sent)/1000, 0))+'K'+'\r', )
-                    sys.stdout.flush()
+                if n_sent % 1000 == 0: #n_sent divides in 10000 without remainder
+                    print  (str(round(float(n_sent)/1000, 0))+'K', end="\r")
+                    time.sleep(0.1)
 
                 # Randomly skip 90% of the sentences to get an even distribution of the data.
                 line=line.rstrip()
@@ -189,7 +190,7 @@ def get_cws(files):
                 for word in words:
                     # Skip empty words and punctuation.
                 
-                    if not bool(re.match(r'^[a-z]+$', word)) or not(len(word))  :
+                    if not bool(re.match(r'^[a-z_]+$', word)) or not(len(word))  : #a-z_ for word 2 phrase
                         continue
                     
                     n_words+=1
@@ -220,17 +221,24 @@ def get_cws(files):
     
 def main():
     
-    #input_files = "/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/word_2phrase_corpus/webbase_phrase2.txt,/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/word_2phrase_corpus/wiki_phrase2.txt,/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/word_2phrase_corpus/billion_phrase2.txt,/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/word_2phrase_corpus/news_2013_phrase2.txt,/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/word_2phrase_corpus/news2012_phrase2.txt"
     #input_files="/home/ira/Google_Drive/IraTechnion/PhD/corpus/english_test"
-    input_files="/home/ira/Google_Drive/IraTechnion/PhD/corpus/mini_english_test.txt"
-    patterns_input_file="selected_patterns.dat"
+    #input_files="/home/ira/Google_Drive/IraTechnion/PhD/corpus/mini_english_test.txt"
+    #input_files="/home/ira.leviant@st.technion.ac.il/clean_corpus/webbase_all_clean.txt,/home/ira.leviant@st.technion.ac.il/clean_corpus/clean_wiki_new.txt,/home/ira.leviant@st.technion.ac.il/clean_corpus/billion_word_clean.txt,/home/ira.leviant@st.technion.ac.il/clean_corpus/news_2012_clean,/home/ira.leviant@st.technion.ac.il/clean_corpus/news_2013_clean"
+    #input_files="/home/ira/Google_Drive/IraTechnion/PhD/corpus/webbase_all_clean.txt,/home/ira/Google_Drive/IraTechnion/PhD/corpus/clean_wiki_new.txt,/home/ira/Google_Drive/IraTechnion/PhD/corpus/billion_word_clean.txt,/home/ira/Google_Drive/IraTechnion/PhD/corpus/news_2012_clean,/home/ira/Google_Drive/IraTechnion/PhD/corpus/news_2013_clean"
     
-    context_vocab_file="context_vocab_out_python.txt"#file mapping context strings to 
-    context_pairs_output_file ="context_file_out_python.txt"
-    dic_file='cws_dictionary_all_pats_python.dat'
-    dic_file_order='cws_dictionary_allpats_python_order.dat'
-    word_vocabulary_output_file='word_vocab_python_allpats_python.dat'
-    mat_file='all_pats_python_mat.npz'
+    
+    input_files="/home/ira/Google_Drive/IraTechnion/PhD/corpus/webbase_phrase2.txt,/home/ira/Google_Drive/IraTechnion/PhD/corpus/billion_phrase2.txt,/home/ira/Google_Drive/IraTechnion/PhD/corpus/wiki_phrase2.txt,/home/ira/Google_Drive/IraTechnion/PhD/corpus/news2012_phrase2.txt,/home/ira/Google_Drive/IraTechnion/PhD/corpus/news_2013_phrase2.txt"
+    #input_files="/home/ira/Google_Drive/IraTechnion/PhD/corpus/example_after_2phrase.txt"
+    
+    patterns_input_file='selected_patterns_py_2.txt'#"selected_patterns_py.txt"
+
+    
+    context_vocab_file="context_vocab_out_python_2prase.txt"#file mapping context strings to 
+    context_pairs_output_file ="context_file_out_python_2phrase.txt"
+    dic_file='cws_dictionary_all_pats_python_2phrase.dat'
+    dic_file_order='cws_dictionary_allpats_python_order_2phrase.dat'
+    word_vocabulary_output_file='word_vocab_python_allpats_python_2phrase.dat'
+    mat_file='all_pats_python_mat_2phrase.npz'
     # Read patterns into a Trie data structure.
     patterns_trie = read_patterns_trie(patterns_input_file)
     
@@ -238,7 +246,7 @@ def main():
         
     #cws_clean={} # dictionary of content words with their counts
     cws_clean = get_cws(ifs)  
-    write_vocab(cws_clean, dic_file)
+    write_vocab(cws_clean, dic_file)  #file with all the cws and their countings
     print ("Finished reading content word dictionary clean its length is:", len(cws_clean))
     
     cws_clean_order={}
@@ -249,7 +257,8 @@ def main():
     
     write_vocab(cws_clean_order, dic_file_order)
     
-    co_mat = lil_matrix((len(cws_clean), len(cws_clean)) )
+    co_mat = lil_matrix((len(cws_clean_order), len(cws_clean_order)) )
+    
      
     n_lines = 0
     #pat_words={}
@@ -266,8 +275,8 @@ def main():
             for line in ifh:
                 n_lines=n_lines+1
                 if n_lines % 10000 == 0: #n_lines divides in 10000 without remainder
-                    print ( str(round(float(n_lines)/1000, 0))+'K'+'\r', )
-                    sys.stdout.flush()
+                    print  (str(round(float(n_lines)/10000, 0))+'K', end="\r")
+                    time.sleep(0.1)
                      
                 line=line.strip()
                 line=line.lower() 
@@ -275,8 +284,9 @@ def main():
                 words=re.findall(r'\w+|[^\w\s]+', line) #this version the same as perl, \w+ - 1 or more word chars (letters, digits or underscores), | - or, [^\w\s] - 1 char other than word / whitespace
                 
                 # Search for patterns starting at each word in the sentence.
-                end_loop=len(words)-2 #maybe error,need to change to -1
+                end_loop=len(words)-2 #maybe error,need to change to -1 - not error
                 for start in range(0,end_loop): 
+                    #print(" start is:", start) 
                     add_patt_instance(words, start, 0, patterns_trie, cws_clean_order, co_mat, word_vocab ,context_vocab, ofh)
                     #add_patt_instance(\@words, $start, 0, $patterns_trie, $cws, $ofh, \%word_vocab, \%context_vocab);
     #===========================================================================
